b74544ee6dfff872cbc47e0498d7fddbe9574e2a
Rafael-Lopez <rafael.lopez@bbd.ca>
2019-04-05 14:33:11 -0400

    Adding Specialities Map Implementation

---
+++ pet-clinic-data/src/main/java/com/lopez/rafael/sfgpetclinic/services/SpecialitiesService.java
@@ -0,0 +1,6 @@
+package com.lopez.rafael.sfgpetclinic.services;
+
+import com.lopez.rafael.sfgpetclinic.model.Speciality;
+
+public interface SpecialitiesService extends CrudService<Speciality, Long> {
+}
